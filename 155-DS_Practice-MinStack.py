b520da4fe5da6320b63ff2177f0bb2256f5bc431
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-02 17:14:38 +0530

    Adding 155-PythonPractise2024-MinStack-v2.0

--- 155-DS_Practice-MinStack.py
+++ 155-DS_Practice-MinStack.py
@@ -17,7 +17,7 @@ minStack.top();    // return 0
 minStack.getMin(); // return -2
 '''
 
-## BRUTE FORCE --> O(1)
+# Approach1: BRUTE FORCE --> O(n)
 class MinStack:
 
     def __init__(self):
@@ -33,8 +33,37 @@ class MinStack:
         if self.array != []:
             return self.array[-1]
 
+    # This is causing the actual O(n) complexity
     def getMin(self) -> int:
         return min(self.array)
+
+# Approach2: MultiStack --> O(1)
+class MinStack:
+
+    def __init__(self):
+        self.array = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.array.append(val)
+        if self.min:
+            if val < self.min[-1]:
+                self.min.append(val)
+        else:
+            self.min.append(val)
+
+    def pop(self) -> None:
+        if self.min[-1] == self.array[-1]:
+            self.min.pop()
+        self.array.pop()
+
+    def top(self) -> int:
+        if self.array:
+            return self.array[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -42,9 +71,12 @@ obj = MinStack()
 obj.push(-2)
 obj.push(0)
 obj.push(-3)
+# obj.pop()
+param_2 = obj.getMin()
 obj.pop()
 param_3 = obj.top()
 param_4 = obj.getMin()
+print(param_2)
 print(param_3)
 print(param_4)
 print(obj.array)
