188d64c27f20c86bfc1c9304e38453d7e155514e
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-25 19:34:24 +0530

    App 1 - ToDoApp

--- PythonPerDayPractice/App1-ToDoApp/gui.py
+++ PythonPerDayPractice/App1-ToDoApp/gui.py
@@ -1,4 +1,24 @@
 import utilities
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
+
+# creating elements
+label = sg.Text("Type in a to do:")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+# creating a window instance
+# layout = "[]" elements in sq brackets will be kept on same row
+window = sg.Window("My To-Do APP", layout=[[label], [input_box,add_button]]) 
+
+# displaying GUI
+window.read()
+
+# displaying hello once GUI functions are performed
+print('hello')
+
+# closing instances & GUI
+window.close()
+
+
 
 
