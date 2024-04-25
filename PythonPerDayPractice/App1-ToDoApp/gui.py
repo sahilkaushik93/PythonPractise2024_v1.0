b98d0c067e22f0b2c0cc7c58ff29591b902eb8e7
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-25 23:06:16 +0530

    App 1 - ToDoApp

--- PythonPerDayPractice/App1-ToDoApp/gui.py
+++ PythonPerDayPractice/App1-ToDoApp/gui.py
@@ -2,21 +2,61 @@ import utilities
 import FreeSimpleGUI as sg
 
 # creating elements
-label = sg.Text("Type in a to do:")
-input_box = sg.InputText(tooltip="Enter todo")
+label1 = sg.Text("Type in a to do:")
+input_box1 = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+
+label2 = sg.Text("List of all to do tasks:")
+input_box2 = sg.Listbox(values=utilities.read_todos(), key="todos",
+                        enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
+
+
+layout = [
+    [label1, input_box1, add_button],
+    [label2],
+    [input_box2, edit_button]
+]
 
 # creating a window instance
 # layout = "[]" elements in sq brackets will be kept on same row
-window = sg.Window("My To-Do APP", layout=[[label], [input_box,add_button]]) 
+window = sg.Window("My To-Do APP", 
+                   layout=layout,
+                   font=('Helvetica', 10)) 
 
-# displaying GUI
-window.read()
+
+# iterating to add multiple value via app
+while True:
+    # displaying GUI
+    event, values = window.read()
+
+    # event looks like a tuple containing value added in dict format
+    print("event: ",event) # event at which value was added (added due to input_button)
+    print("value: ", values) # value added with a key of "todo" (key given in input_button definition)
+
+    match event:
+        case "Add":
+            # adding add functionality [updating todos.txt in background]
+            todos = utilities.read_todos()
+            new_todo = values['todo']
+            todos.append(new_todo)
+            utilities.write_todos(todos)
+
+            # making things appear live by updating values in windows instance
+            window["todos"].update(values['todo'])
+
+        case "Edit":
+            print(values)
+        case sg.WIN_CLOSED:
+            break
+
+ 
+    
 
 # displaying hello once GUI functions are performed
 print('hello')
 
-# closing instances & GUI
+# closing instances & GUI 
 window.close()
 
 
--- PythonPerDayPractice/App1-ToDoApp/utilities.py
+++ PythonPerDayPractice/App1-ToDoApp/utilities.py
@@ -1,5 +1,5 @@
 # utility functions
-def read_todos(todos_loc):
+def read_todos(todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
     '''
     functions will be reading todos file
     todos_loc : type <str>
@@ -9,7 +9,18 @@ def read_todos(todos_loc):
     file1.close()
     return todos
 
+def write_todos(txt, todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
+    '''
+    functions will be writing todos in todo.txt file
+    todos_loc : type <str>
+    '''
+    with open(todos_loc,"w") as file1:
+        todos = file1.writelines(str(txt) + "\n")
+    file1.close()
+    return todos
+
+
 
 
 if __name__ == "__main__":
-    print("utilities specific print commands won't run in wrapper main script")                                                                                                               
+    print("utilities.py specific print commands won't run in wrapper main script")                                                                                                               
