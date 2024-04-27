4a445e49b5a722d1dcd43e3642e8480601230850
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-27 12:22:46 +0530

    App 1 - ToDoApp

--- PythonPerDayPractice/App1-ToDoApp/gui.py
+++ PythonPerDayPractice/App1-ToDoApp/gui.py
@@ -1,9 +1,20 @@
+## Import Libraries
+# sys to access "utilities.py" present in some other folder structure
+# "utilities" to access all supporting utility functions
+# "FreeSimpleGUI" to create Desktop App Interface ("PySimpleGUI" is paid version of "FreeSimpleGUI")
+# "time" to add displaying time feature on APP
 import sys
 sys.path.append('E:\Job & Interview Kit\Revision Material\DS & Algos - Python & JavaScript\PythonPractise2024\PythonPerDayPractice')
 import utilities
 import FreeSimpleGUI as sg
+import time
 
-# creating elements
+# Applying theme (go on google & search "PySimpleGUI themes images")
+sg.theme("Black")
+
+# creating window elements
+now = time.strftime("%b %d, %Y %H:%M:%S")
+clock = sg.Text(key="clock")
 label1 = sg.Text("Type in a to do:")
 input_box1 = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -15,8 +26,9 @@ edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
-
+# giving layout of elements to be displayed in windows 
 layout = [
+    [clock],
     [label1], 
     [input_box1],
     [label2],
@@ -34,7 +46,10 @@ window = sg.Window("My To-Do APP",
 # iterating to add multiple value via app
 while True:
     # displaying GUI
-    event, values = window.read()
+    event, values = window.read(timeout=100)
+
+    # displaying clock on window
+    window["clock"].update(value=now)
 
     # event looks like a tuple containing value added in dict format
     print("1-event: ",event) # event at which value was added (added due to input_button)
@@ -56,7 +71,7 @@ while True:
             else:
                 new_todo = values['todo'] + "\n"
             
-            todos.append(new_todo)
+            todos.append(f"{now} : {new_todo}")
             print("new todos: ",todos)
             
             utilities.write_todos(todos)
@@ -68,12 +83,15 @@ while True:
             # adding edit functionality [updating todos.txt in background]
             todos = utilities.read_todos()
 
-            edit_todo = values['todos'][0]
-            edit_index = todos.index(edit_todo)
-            new_todo = values['todo'] + "\n"
-            todos[edit_index] = new_todo
-            
-            utilities.write_todos(todos)
+            try:
+                edit_todo = values['todos'][0]
+                edit_index = todos.index(edit_todo)
+                new_todo = values['todo'] + "\n"
+                todos[edit_index] = new_todo
+                
+                utilities.write_todos(todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 10))
 
             # making things appear live by updating values in windows instance
             window["todos"].update(values=todos)
@@ -83,15 +101,18 @@ while True:
             window['todo'].update(value=values['todos'][0])
 
         case "Complete":
-            todos = utilities.read_todos()
-            print("read todos: ", todos)
-            val = values['todos'][0]
-            print("val:", val)
-            # val_index = todos.index(val)
-            # todos.pop(val_index)
-            todos.remove(val)
-            print("write todos:", todos)
-            utilities.write_todos(todos)
+            try:
+                todos = utilities.read_todos()
+                print("read todos: ", todos)
+                val = values['todos'][0]
+                print("val:", val)
+                # val_index = todos.index(val)
+                # todos.pop(val_index)
+                todos.remove(val)
+                print("write todos:", todos)
+                utilities.write_todos(todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 10))    
 
             # making things appear live by updating values in windows instance
             window["todos"].update(values=todos) # changes will appear in list box
