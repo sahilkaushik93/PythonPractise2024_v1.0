ac3e3d15f690d2b03992ef8808181a8d1a63581f
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-27 10:03:59 +0530

    App3 - File Compressor

--- PythonPerDayPractice/App3-FileCompressorApp/gui.py
+++ PythonPerDayPractice/App3-FileCompressorApp/gui.py
@@ -1,23 +1,35 @@
+import sys
+sys.path.append('E:\Job & Interview Kit\Revision Material\DS & Algos - Python & JavaScript\PythonPractise2024\PythonPerDayPractice')
+import utilities
 import FreeSimpleGUI as sg # for documentation search PySimpleGUI
 
 # elements
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output")
 
 # window instance
 window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
-                                              [compress_button]])
+                                              [compress_button, output_label]])
 
 # using the window instance, opening the app & performing actions
-window.read()
+while True:
+    event, values = window.read()
+    print("event:",event)
+    print("values:",values)
+    print("file: ", values['files'].split(";"))
+    utilities.make_archive(filepaths=values['files'].split(";"), dest_dir=values['folder'])
+    window['output'].update(value = "Compressed Successfully!")
 
-# closing the window instance
-window.close()
+    # closing the window instance
+    if sg.WIN_CLOSED:
+        exit()
+
--- PythonPerDayPractice/utilities.py
+++ PythonPerDayPractice/utilities.py
@@ -1,4 +1,7 @@
 # utility functions
+import zipfile
+import pathlib
+
 def read_todos(todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
     '''
     functions will be reading todos file
@@ -15,12 +18,17 @@ def write_todos(txt, todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
     '''
     with open(todos_loc,"w") as file1:
         file1.writelines(txt)
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path, 'w') as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
     
 if __name__ == "__main__":
     print(read_todos())
+    print("utilities.py specific print commands won't run in wrapper main script")
 
-
-
-
-if __name__ == "__main__":
-    print("utilities.py specific print commands won't run in wrapper main script")                                                                                                               
+    
+                                                                                                                   
