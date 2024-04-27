9fdb3f70e1bc2bc5509f85790b4f46beede6de20
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-04-27 14:19:32 +0530

    App 3 - FileCompressorApp

--- PythonPerDayPractice/App3-FileCompressorApp/gui.py
+++ PythonPerDayPractice/App3-FileCompressorApp/gui.py
@@ -4,32 +4,48 @@ import utilities
 import FreeSimpleGUI as sg # for documentation search PySimpleGUI
 
 # elements
-label1 = sg.Text("Select files to compress:")
+label1 = sg.Text("Select Source:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose", key="files")
+choose_button1 = sg.FilesBrowse("Choose", key="Source")
 
-label2 = sg.Text("Select destination folder:")
+label2 = sg.Text("Select Target:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose", key="folder")
+choose_button2 = sg.FolderBrowse("Choose", key="Target")
 
 compress_button = sg.Button("Compress")
-output_label = sg.Text(key="output")
+extract_button = sg.Button("Extract")
+exit_button = sg.Button("Exit")
+output_label = sg.Text(key="output", text_color="Red")
+
+layout = [
+    [label1, input1, choose_button1],
+    [label2, input2, choose_button2],
+    [compress_button, extract_button, exit_button, output_label]
+    ]
 
 # window instance
-window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
-                                              [label2, input2, choose_button2],
-                                              [compress_button, output_label]])
+window = sg.Window("File Compressor", layout=layout)
 
 # using the window instance, opening the app & performing actions
 while True:
     event, values = window.read()
     print("event:",event)
     print("values:",values)
-    print("file: ", values['files'].split(";"))
-    utilities.make_archive(filepaths=values['files'].split(";"), dest_dir=values['folder'])
-    window['output'].update(value = "Compressed Successfully!")
+    
 
-    # closing the window instance
-    if sg.WIN_CLOSED:
-        exit()
+    match event:
+        case "Compress":
+            utilities.make_archive(filepaths=values['Source'].split(";"), dest_dir=values['Target'])
+            window['output'].update(value = "Compressed Successfully!")
+        case "Extract":
+            utilities.extract_archive(archivepath=values['Source'], dest_dir=values['Target'])
+            window['output'].update(value = "Extracted Successfully!")
+        case "Exit":
+            exit()
+        case sg.WIN_CLOSED:
+            exit()
+
+    # # closing the window instance
+    # if sg.WIN_CLOSED:
+    #     exit()
 
--- PythonPerDayPractice/utilities.py
+++ PythonPerDayPractice/utilities.py
@@ -11,6 +11,7 @@ def read_todos(todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
         todos = file1.readlines()
     return todos
 
+
 def write_todos(txt, todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
     '''
     functions will be writing todos in todo.txt file
@@ -19,16 +20,28 @@ def write_todos(txt, todos_loc='./PythonPerDayPractice/App1-ToDoApp/todos.txt'):
     with open(todos_loc,"w") as file1:
         file1.writelines(txt)
 
+
 def make_archive(filepaths, dest_dir):
+    '''
+    It helps in compressing the selected files into a folder
+    '''
     dest_path = pathlib.Path(dest_dir, "compressed.zip")
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
             filepath = pathlib.Path(filepath)
             archive.write(filepath, arcname=filepath.name)
-    
+
+
+def extract_archive(archivepath, dest_dir):
+    '''
+    It helps in extracting all the files present in a compressed folder
+    '''
+    with zipfile.ZipFile(archivepath, 'r') as archive:
+        archive.extractall(dest_dir)
+
+
 if __name__ == "__main__":
     print(read_todos())
     print("utilities.py specific print commands won't run in wrapper main script")
 
-    
-                                                                                                                   
+
