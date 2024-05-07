8cd637415671ecfd5ee08baf7b2ca501017fe4f4
sahilkaushik93 <kaushiksahil1412@gmail.com>
2024-05-07 23:05:04 +0530

    App 8 - Historical Weather App

---
+++ PythonPerDayPractice/App8-HistoricalWeatherAPI/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+
+print(os.getcwd())
+
+loc = "E:\Job & Interview Kit\Revision Material\DS & Algos - Python & JavaScript\PythonPractise2024\PythonPerDayPractice\App8-HistoricalWeatherAPI"
+
+# creating a website object with Flask
+app = Flask("Website", template_folder=f"{loc}\\templates")
+
+# decorator connects app method with home function i.e. 
+# "/home" url connected with "tutorial.html"
+@app.route("/home")
+def home():
+    return render_template(f"tutorial.html")
+
+@app.route("/about/")
+def about():
+    return render_template(f"about.html")
+
+# debug = True will give you a feature of displaying errors on webpage
+app.run(debug=True)
